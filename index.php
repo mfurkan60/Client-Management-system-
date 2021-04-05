4d473ffbb01c2dbdf94251db9b993fa20b01a611
Mert ERGÜDEN <mfurkanergdn@hotmail.com>
2021-04-05 22:13:56 +0300

    added template

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include "include/header.php" ?>
+ 
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+         
+         
+        <div class="row">
+        
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include "include/footer.php" ?>
